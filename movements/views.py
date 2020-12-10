300a1d3128f086e06401e56581230549e571a308
Angel <calvo.monzon.angel@gmail.com>
2020-12-10 12:26:14 +0100

    cambios basedatos

--- movements/views.py
+++ movements/views.py
@@ -1,24 +1,29 @@
 from movements import app
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect
 import csv
 
-@app.route("/")
+@app.route('/')
 def listaIngresos():
     fIngresos = open("movements/data/basededatos.csv", "r")
-    csvReader = csv.reader(fIngresos, delimiter = ",", quotechar = '"')
+    csvReader = csv.reader(fIngresos, delimiter=',', quotechar='"')
     ingresos = list(csvReader)
-    sumador = 0
-    for ingreso in ingresos:
-        sumador += float(ingreso[2])
-    
-    print(ingresos)
-    
-    return render_template("movementList.html", datos=ingresos, total=sumador)
 
-@app.route("/crealta", methods=["GET", "POST"])
-def nuevoIngreso ():
-    if request.method == "POST":
-        print(request.form)
+    print(ingresos)
+
+    total = 0
+    for ingreso in ingresos:
+        total += float(ingreso[2])
+
+    return render_template("movementList.html",datos=ingresos, total=total)
+
+@app.route('/creaalta', methods=['GET', 'POST'])
+def nuevoIngreso():
+    if request.method == 'POST':
+        fIngresos = open("movements/data/basededatos.csv", "a", newline="")
+        csvWriter = csv.writer(fIngresos, delimiter=',', quotechar='"')
+        csvWriter.writerow([request.form.get('fecha'), request.form.get('concepto'), request.form.get('cantidad')])
+        return redirect(url_for('listaIngresos'))
         
-    
+
+
     return render_template("alta.html")
