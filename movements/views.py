201c5d2d9798856ffd60174d4130726de756b2f0
Angel <calvo.monzon.angel@gmail.com>
2020-12-11 00:29:19 +0100

    bd

--- movements/views.py
+++ movements/views.py
@@ -1,24 +1,44 @@
 from movements import app
 from flask import render_template, request, url_for, redirect
 import csv
+import sqlite3
 
 @app.route('/')
 def listaIngresos():
-    fIngresos = open("movements/data/basededatos.csv", "r")
+    conn = sqlite3.connect("movements/data/basededatos.db")
+    c = conn.cursor()
+    
+    c.execute("SELECT fecha, concepto, cantidad, id FROM movimientos;")
+    
+    """fIngresos = open("movements/data/basededatos.csv", "r")
     csvReader = csv.reader(fIngresos, delimiter=',', quotechar='"')
-    ingresos = list(csvReader)
+    ingresos = list(csvReader)"""
 
-    print(ingresos)
+    ingresos = c.fetchall()
 
     total = 0
     for ingreso in ingresos:
         total += float(ingreso[2])
 
-    return render_template("movementList.html",datos=ingresos, total=total)
+    return render_template("movementsList.html",datos=ingresos, total=total)
 
 @app.route('/creaalta', methods=['GET', 'POST'])
 def nuevoIngreso():
     if request.method == 'POST':
+        
+        conn = sqlite3.connect("movements/data/basededatos.db")
+        c = conn.cursor()
+    
+        c.execute("INSERT INTO movimientos (cantidad, concepto, fecha) VALUES (?,?,?);", 
+                  (
+                      float(request.form.get("cantidad")), 
+                      request.form.get("concepto"), 
+                      request.form.get("fecha")
+                      ))
+        
+        conn.commit()
+        
+        
         fIngresos = open("movements/data/basededatos.csv", "a", newline="")
         csvWriter = csv.writer(fIngresos, delimiter=',', quotechar='"')
         csvWriter.writerow([request.form.get('fecha'), request.form.get('concepto'), request.form.get('cantidad')])
@@ -27,3 +47,5 @@ def nuevoIngreso():
 
 
     return render_template("alta.html")
+
+@app.route("/modifica/<id>", methods=["GET", "POST"])
