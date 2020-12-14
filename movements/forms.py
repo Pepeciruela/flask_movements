659eb039fb3b9e22808e2f676181339cd544e676
Angel <calvo.monzon.angel@gmail.com>
2020-12-15 00:38:01 +0100

    cambios css

---
+++ movements/forms.py
@@ -0,0 +1,14 @@
+
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, DateField, StringField, FloatField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+
+
+class MovementForm(FlaskForm):
+    id = IntegerField('id')
+    fecha = DateField('Fecha', validators=[DataRequired()])
+    concepto = StringField('Concepto', validators=[DataRequired(), Length(min=10, message="El concepto debe tener más de 10 caracteres")])
+    cantidad = FloatField('Cantidad', validators=[DataRequired()])
+
+    submit = SubmitField('Aceptar')
--- movements/views.py
+++ movements/views.py
@@ -1,91 +1,97 @@
 from movements import app
+from movements.forms import MovementForm
 from flask import render_template, request, url_for, redirect
 import csv
 import sqlite3
 
-BDFILE = "movements/data/basededatos.db"
+DBFILE = app.config['DBFILE']
 
-def consulta(query, params):
-    conn = sqlite3.connect(BDFILE) # crear conexión
-    c = conn.cursor() # crear cursor
-    
-    c.execute(query, params) #ejecutar los parámetros
-    filas = fetchall()
-    con.commit()
+def consulta(query, params=()):
+    conn = sqlite3.connect(DBFILE)
+    c = conn.cursor()
+    '''
+    'SELECT * FROM TABLA' -> [(),(), (),]
+    'SELECT * FROM TABLA VACIA ' -> []
+    'INSERT ...' -> []
+    'UPDATE ...' -> []
+    'DELETE ...' -> []
+    '''
+
+    c.execute(query, params)
+    conn.commit()
+
+    filas = c.fetchall()
+    print(filas)
+
     conn.close()
+
+
     if len(filas) == 0:
-        return None
-    
+        return filas
+
     columnNames = []
-    for columnNames in c.description: #se añaden los nombres en la lista
-        columnNames.append(columnNames[0])
-    
+    for columnName in c.description:
+        columnNames.append(columnName[0])
+
     listaDeDiccionarios = []
-    
+
     for fila in filas:
         d = {}
         for ix, columnName in enumerate(columnNames):
             d[columnName] = fila[ix]
-            listaDeDiccionarios.append(d)
-    
-    if len (listaDeDiccionarios) == 1:
-        return listaDeDiccionarios [0]
-    else:
-        return listaDeDiccionarios
+        listaDeDiccionarios.append(d)
+
+    return listaDeDiccionarios
 
 @app.route('/')
 def listaIngresos():
-    ingresos = consulta("SELECT fecha, concepto, cantidad, id FROM movimientos;") or [] # ejecutar lo seleccionado
+
+    ingresos = consulta('SELECT fecha, concepto, cantidad, id FROM movimientos;')
 
     total = 0
     for ingreso in ingresos:
-        total += float(ingreso["cantidad"])
-        
-    conn.close()
+        total += float(ingreso['cantidad'])
+
 
     return render_template("movementsList.html",datos=ingresos, total=total)
 
 @app.route('/creaalta', methods=['GET', 'POST'])
 def nuevoIngreso():
+
+    form = MovementForm(request.form)
+    
     if request.method == 'POST':
-        
-        conn = sqlite3.connect(BDFILE)
-        c = conn.cursor()
-    
-        c.execute("INSERT INTO movimientos (cantidad, concepto, fecha) VALUES (?,?,?);", 
-                  (
-                      float(request.form.get("cantidad")), 
-                      request.form.get("concepto"), 
-                      request.form.get("fecha")
-                      ))
-        
-        
-        return redirect(url_for("listaingresos"))
+        if form.validate():
+            consulta('INSERT INTO movimientos (cantidad, concepto, fecha) VALUES (?, ? ,? );', 
+                    (
+                        float(form.cantidad,data),
+                        form.concepto.data,
+                        form.fecha.data
+                    )
+            )
+            return redirect(url_for('listaIngresos'))
 
-    return render_template("alta.html")
+    return render_template("alta.html", form=form)
 
-@app.route("/modifica/<id>", methods=["GET", "POST"])
-def modificaingreso(id):
-    conn = sqlite3.connect(BDFILE)
+
+@app.route("/modifica/<id>", methods=['GET', 'POST'])
+def modificaIngreso(id):
+    conn = sqlite3.connect(DBFILE)
     c = conn.cursor()
-    
-    if request.method == "GET":
-        registro = consulta("SELECT fecha, concepto, cantidad, id FROM movimientos where id = ?", (id,))
-        registro = c.fetchone() #recuperas el registro
-    
-        conn.close()
-    
+
+    if request.method == 'GET':
+
+        registro = consulta('SELECT fecha, concepto, cantidad, id FROM movimientos where id = ?', (id,))[0] 
+
         return render_template("modifica.html", registro=registro)
     else:
-        c.execute("UPDATE movimientos SET fecha = ?, concepto = ?, cantidad = ? WHERE id =?", 
-                  (request.form.get("fecha"),
-                   request.form.get ("concepto"),
-                   float(request.form.get("cantidad")),
+        consulta('UPDATE movimientos SET fecha = ?, concepto= ?, cantidad = ? WHERE id = ?',
+                  (request.form.get('fecha'),
+                   request.form.get('concepto'),
+                   float(request.form.get('cantidad')),
                    id
-                   )
                   )
-        conn.commit() #ratificar el cambio
-        conn.close () #cierra conexión con la base de datos
-        
-        return redirect(url_for("listaIngresos")) # devuelve la respuesta al navegador y le dice que lo siguiente es la petición a 
-        
+        )
+
+        return redirect(url_for("listaIngresos"))
+ 
